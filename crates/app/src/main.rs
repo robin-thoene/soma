f1fe203528ee64cf14d51f4705378d64986e10bb
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-12-12 22:42:43 +0100

    chore(wip): get data to rust code

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -5,6 +5,12 @@ slint::include_modules!();
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
+    ui.on_submit(move |weight, body_fat, muscle_mass| {
+        println!("{}", weight);
+        println!("{}", body_fat);
+        println!("{}", muscle_mass);
+    });
+
     ui.run()?;
 
     Ok(())
