fcce1b400fb50d5544da61b75d5bb4be125bd639
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-12-12 21:20:45 +0100

    chore: rm not needed code

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,6 +1,3 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use std::error::Error;
 
 slint::include_modules!();
@@ -20,4 +17,3 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
