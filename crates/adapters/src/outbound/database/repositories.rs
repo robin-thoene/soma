f7fb5863e360980a9aa1afb7ef9269768ad821e6
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-12-14 23:11:10 +0100

    chore(wip): impl repo for creating measurements in db

--- crates/adapters/src/outbound/database/repositories.rs
+++ crates/adapters/src/outbound/database/repositories.rs
@@ -1,28 +1,42 @@
-use domain::models::Measurement;
+use async_trait::async_trait;
+use chrono::Utc;
 use ports::outbound::measurement_port::MeasurementPort;
 use sqlx::SqlitePool;
 
 pub struct MeasurementRepository {
-    _db_conn: SqlitePool,
+    db_conn: SqlitePool,
 }
 
 impl MeasurementRepository {
     pub fn new(db_conn: SqlitePool) -> Self {
-        Self { _db_conn: db_conn }
+        Self { db_conn }
     }
 }
 
+#[async_trait]
 impl MeasurementPort for MeasurementRepository {
-    fn create_measurement(
+    async fn create_measurement(
+        &self,
         weight_kg: f32,
         body_fat_pct: Option<f32>,
         muscle_mass_pct: Option<f32>,
-    ) -> Result<Measurement, String> {
-        // TODO: implement and remove debug fragments
-        println!(
-            "Insert new measurement with {:?}, {:?}, {:?}",
-            weight_kg, body_fat_pct, muscle_mass_pct
-        );
-        todo!()
+    ) -> Result<(), String> {
+        let timestamp = Utc::now();
+        let result = sqlx::query(
+            r#"
+                INSERT INTO todos ( measurements )
+                VALUES ( ?1, ?2, ?3, ?4 )
+            "#,
+        )
+        .bind(timestamp.to_rfc3339())
+        .bind(weight_kg)
+        .bind(body_fat_pct)
+        .bind(muscle_mass_pct)
+        .execute(&self.db_conn)
+        .await;
+        match result {
+            Ok(_) => Ok(()),
+            Err(err) => Err(err.to_string()), // TODO: better error handling
+        }
     }
 }
--- crates/ports/src/outbound/measurement_port.rs
+++ crates/ports/src/outbound/measurement_port.rs
@@ -1,9 +1,11 @@
-use domain::models::Measurement;
+use async_trait::async_trait;
 
-pub trait MeasurementPort {
-    fn create_measurement(
+#[async_trait]
+pub trait MeasurementPort: Send + Sync {
+    async fn create_measurement(
+        &self,
         weight_kg: f32,
         body_fat_pct: Option<f32>,
         muscle_mass_pct: Option<f32>,
-    ) -> Result<Measurement, String>; // TODO: better error types/handling
+    ) -> Result<(), String>; // TODO: better error types/handling
 }
