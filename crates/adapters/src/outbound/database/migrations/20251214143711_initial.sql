2279d9d147e4f892008710c294b3afb9482fbc6c
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-12-14 20:13:41 +0100

    chore(db): add sqlx and add initial sqlite migration

---
+++ crates/adapters/src/outbound/database/migrations/20251214143711_initial.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS measurements
+(
+    id                  INTEGER PRIMARY KEY NOT NULL,
+    utc_time            TEXT                NOT NULL,
+    weight_kg           REAL                NOT NULL,
+    body_fat_pct        REAL                NOT NULL,
+    muscle_mass_pct     REAL                NOT NULL
+);
